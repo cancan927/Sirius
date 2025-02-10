91c23a164625928caca069cdbc86cf2f03f020cf
Zhang Cancan <felicityzhangcc@foxmail.com>
2025-02-10 17:53:19 +0800

    定义数据磁盘结构以及编码操作

--- data/log_record.go
+++ data/log_record.go
@@ -1,8 +1,27 @@
 package data
 
-// 数据内存索引，主要是描述数据在磁盘上的位置
+type LogRecordType = uint8
 
+const (
+	LogRecordNormal LogRecordType = iota
+
+	LogRecordDeleted
+)
+
+// LogRecordPos 内存数据索引，主要是内存中维护的描述数据在磁盘上的位置的结构
 type LogRecordPos struct {
 	Fid    uint32 // 文件id,表示数据存储在哪个文件中
 	Offset int64  // 偏移，表示数据在文件中的哪个位置
+}
+
+// LogRecord 记录到磁盘的数据记录
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType // 记录类型是否被删除
+}
+
+// EncodeLogRecord 编码LogRecord,返回字节数组以及长度
+func EncodeLogRecord(record *LogRecord) ([]byte, int64) {
+	return nil, 0
 }
