7f99b0a11ee3b9952a169fa64e8d4595c71c49de
Zhang Cancan <felicityzhangcc@foxmail.com>
2025-02-15 02:16:31 +0800

    增加err

--- error.go
+++ error.go
@@ -3,8 +3,11 @@ package sirius
 import "errors"
 
 var (
-	ErrKeyIsEmpty        = errors.New("key is empty")
-	ErrIndexUpdateFailed = errors.New("index update failed")
-	ErrKeyNotFound       = errors.New("key not found")
-	ErrDataFileNotFound  = errors.New("data file not found")
+	ErrKeyIsEmpty             = errors.New("key is empty")
+	ErrIndexUpdateFailed      = errors.New("index update failed")
+	ErrKeyNotFound            = errors.New("key not found")
+	ErrDataFileNotFound       = errors.New("data file not found")
+	ErrDirPathIsEmpty         = errors.New("dir path is empty")
+	ErrDataFileSizeZero       = errors.New("data file size must be greater than 0")
+	ErrDataDirectoryCorrupted = errors.New("data directory corrupted")
 )
